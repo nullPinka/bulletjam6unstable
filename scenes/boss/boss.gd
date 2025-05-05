915f5bc0381b2b73211851b12a826c6fcb49317f
nullpinka <nullpinka@gmail.com>
2025-05-05 15:17:07 -0500

    boss attack progress

--- scenes/boss/boss.gd
+++ scenes/boss/boss.gd
@@ -3,11 +3,18 @@ extends Node2D
 var r = 6
 var spawncirc = PI * r * r
 
-func _ready():
-	var newbull = load("res://scenes/bullets/linearbullet.tscn")
-	for i in range(0, 359, 4):
+func circumatt(from, to, gap, bullet : String, speed = 1):
+	var newbull = load("res://scenes/bullets/" + bullet + ".tscn")
+	for i in range(from, to, gap):
 		var tempbull = newbull.instantiate()
 		tempbull.scale = Vector2(0.5,0.5)
 		tempbull.position = spawncirc * Vector2.from_angle(i * (PI / 180))
 		tempbull.anglefromboss = Vector2.from_angle(i * (PI / 180))
+		tempbull.speed = speed
 		add_child(tempbull)
+	return;
+
+func _ready():
+	circumatt(0, 180, 8, "linearbullet")
+	circumatt(5, 185, 7, "linearbullet", 3)
+	circumatt(-5, 175, 6, "linearbullet", 6)
--- scenes/bullets/linearbullet.gd
+++ scenes/bullets/linearbullet.gd
@@ -3,13 +3,15 @@ extends "bullet.gd"
 var movefunc = func(p0, p1, t) : return p0.lerp(p1, t)
 var endp = Vector2.ZERO
 var anglefromboss = Vector2.ZERO
+var speed = 1
 
 func rng_fade():
 	randomize()
-	var time = randf()
-	var period = randi() + randf()
+	var fadeout = randf()
+	var time = randi() + randf()
+	var freq = randi()
 	return;
 
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
-	position += Vector2(1,1).normalized() * anglefromboss
+	position += Vector2(1,1).normalized() * anglefromboss * speed
